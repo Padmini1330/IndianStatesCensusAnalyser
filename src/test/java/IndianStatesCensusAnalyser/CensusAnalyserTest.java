fe43b4b19e17b4439ad99f4b431a6a15f8bec303
padmini v <padmini.v@perfios.com>
2021-09-21 18:43:11 +0530

    REFACTORED: added test method to verify the file with incorrect file format

--- src/test/java/IndianStatesCensusAnalyser/CensusAnalyserTest.java
+++ src/test/java/IndianStatesCensusAnalyser/CensusAnalyserTest.java
@@ -11,13 +11,16 @@ public class CensusAnalyserTest
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
   	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
   	private static final String INDIAN_CSV_STATE_PATH="./src/test/resources/IndiaStateCode.csv";
+  	private static final String INCORRECT_FILE_FORMAT = "./src/test/resources/CensusDataInWrongFormat.txt";
+  	
+  	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+	ExpectedException exceptionRule = ExpectedException.none();
   	
     @Test
     public void givenStateCensusCSVFile_ShouldReturn_CorrectNumberOfRecords() 
     {
         try 
         {
-            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndianCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
         } 
@@ -32,8 +35,6 @@ public class CensusAnalyserTest
     {
 		try 
 		{
-			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyserException.class);
 			censusAnalyser.loadIndianCensusData(WRONG_CSV_FILE_PATH);
 		} 
@@ -43,5 +44,20 @@ public class CensusAnalyserTest
 			e.printStackTrace();
 		}
 	}
+    
+    @Test
+    public void givenStateCensusCSVFile_WhenCorrectButWrongFileFormat_ShouldThrowException() 
+    {
+		try 
+		{
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.loadIndianCensusData(INCORRECT_FILE_FORMAT);
+		} 
+		catch (CensusAnalyserException e) 
+		{
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT, e.type);
+			e.printStackTrace();
+		}
+    }
     
 }
